065ccf7d8b237dd16771f6c50094965aa8bf45e6
AngieArlanti <angeles.arlanti@mercadolibre.com>
2017-02-03 11:38:00 -0300

    Credentials updated

--- example/src/main/java/com/mercadopago/examples/utils/ExamplesUtils.java
+++ example/src/main/java/com/mercadopago/examples/utils/ExamplesUtils.java
@@ -35,9 +35,9 @@ public class ExamplesUtils {
     public static final int CARD_REQUEST_CODE = 13;
 
     // * Merchant public key
-    public static final String DUMMY_MERCHANT_PUBLIC_KEY = "APP_USR-0aa4e281-002e-4666-a1cd-95df12595014";
+    public static final String DUMMY_MERCHANT_PUBLIC_KEY = "TEST-bdd07ce7-a827-45e7-802f-72f0162b9c8c";
 
-    public static final String DUMMY_MERCHANT_PUBLIC_KEY_EXAMPLES_SERVICE = "APP_USR-0aa4e281-002e-4666-a1cd-95df12595014";
+    public static final String DUMMY_MERCHANT_PUBLIC_KEY_EXAMPLES_SERVICE = "TEST-bdd07ce7-a827-45e7-802f-72f0162b9c8c";
     // DUMMY_MERCHANT_PUBLIC_KEY_AR = "444a9ef5-8a6b-429f-abdf-587639155d88";
     // DUMMY_MERCHANT_PUBLIC_KEY_BR = "APP_USR-f163b2d7-7462-4e7b-9bd5-9eae4a7f99c3";
     // DUMMY_MERCHANT_PUBLIC_KEY_MX = "6c0d81bc-99c1-4de8-9976-c8d1d62cd4f2";
