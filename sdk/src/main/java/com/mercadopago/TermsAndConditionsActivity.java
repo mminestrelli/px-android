8bd2dd0a6b0c1edf8d34ffb42f2a4f23323d94b8
AngieArlanti <angeles.arlanti@mercadolibre.com>
2017-02-07 15:05:28 -0300

    MCO's Terms and conditions added

--- sdk/src/main/java/com/mercadopago/TermsAndConditionsActivity.java
+++ sdk/src/main/java/com/mercadopago/TermsAndConditionsActivity.java
@@ -108,6 +108,8 @@ public class TermsAndConditionsActivity extends MercadoPagoActivity {
             mTermsAndConditionsWebView.loadUrl("https://www.mercadopago.com.mx/ayuda/terminos-y-condiciones_715");
         } else if ("MLB".equals(mSiteId)) {
             mTermsAndConditionsWebView.loadUrl("https://www.mercadopago.com.br/ajuda/termos-e-condicoes_300");
+        } else if ("MCO".equals(mSiteId)) {
+            mTermsAndConditionsWebView.loadUrl("https://www.mercadopago.com.co/ayuda/terminos-y-condiciones_299");
         } else {
             finish();
         }
